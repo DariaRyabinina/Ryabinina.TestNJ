39129dd1b9fbce07d93aa069741fd05558e2568a
Daria <ryabinina@i-teco.ru>
2020-03-19 22:45:23 +0300

    changes 2

--- src/test/java/org/DariaRyabinina/ComplexTest.java
+++ src/test/java/org/DariaRyabinina/ComplexTest.java
@@ -7,60 +7,17 @@ import org.testng.annotations.*;
 public class ComplexTest {
     private static final Logger LOG = LoggerFactory.getLogger(ComplexTest.class);
 
-    @BeforeClass
-    public void beforComplexclass() {
-        LOG.info("BeforComplexclass complete");
+
+    @DataProvider(name = "Name")
+
+    private Object[][] dataProvider() {
+        return new Object[][]{{"Василий", 25}, {"Николай", 30}};
     }
 
-    @AfterClass
-    public void afterComplexClass() {
-        LOG.info("After Complex class complete");
+    @Test(groups = "testing", dataProvider = "Name")
+    public void paramTest(String name, int age) {
+        String output = String.format("Name: '%s' | Age: '%d'", name, age);
+        LOG.info(output);
     }
 
-    @BeforeMethod
-    public void beforeComplexMethod() {
-        LOG.info("Before Complex Method  complete");
-
-    }
-
-    @AfterMethod
-    public void afterComplexMethod() {
-        LOG.info("After ComplexMethod  complete");
-
-    }
-
-    @BeforeSuite
-    public void beforeSuite() {
-        LOG.info("Before Suite complete");
-
-    }
-
-    @Test(enabled = false)
-    public void complexTest1() {
-        LOG.info("ComplexTest1 complete");
-
-    }
-
-    @Test
-    public void complexTest2() {
-
-        LOG.info("ComplexTest2 complete");
-    }
-
-    @DataProvider(name="Name")
-
-    private Object[][] dataProvider(){
-        return new Object[][]{{"Василий", 25},{"Николай",30}};
-    }
-
-    @Test(dataProvider="Name")
-    public void paramTest(String name, int age){
-String output= String.format("Name: '%s' | Age: '%d'", name, age);
-LOG.info(output);
-    }
-
-    @AfterSuite
-    public void afterSuite() {
-        LOG.info("After Suite complete");
-    }
 }
--- src/test/java/org/DariaRyabinina/SimpleTest.java
+++ src/test/java/org/DariaRyabinina/SimpleTest.java
@@ -7,37 +7,9 @@ import org.testng.annotations.*;
 public class SimpleTest {
     private static final Logger LOG = LoggerFactory.getLogger(SimpleTest.class);
 
-    @BeforeClass
-    public void beforSimpleclass() {
-        LOG.info("Before Simple class complete");
-    }
-
-    @AfterClass
-    public void afterSimpleClass() {
-        LOG.info("After Simple class complete");
-    }
-
-    @BeforeMethod
-    public void beforeSimpleMethod() {
-        LOG.info("Before Simple Method complete");
-
-    }
-
-    @AfterMethod
-    public void afterSimpleMethod() {
-        LOG.info("After Simple Method complete");
-    }
-
-    @Test(dependsOnMethods ="simpleTest2" )
+    @Test
     public void simpleTest1() {
         LOG.info("Simple Test1 complete");
-
     }
 
-    @Test
-    public void simpleTest2() {
-        LOG.info("Simple Test2 complete");
-
-
-    }
 }
